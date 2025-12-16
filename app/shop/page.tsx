be6e72ccb903d1ea2213fb9bb2b3379094f0c99d
Jonathan Grossman <jon@jonegrossman.com>
2025-12-16 17:54:12 +0200

    removed old shop products

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import { shop } from "@/lib/constants";
 import { ShoppingBag } from "lucide-react";
-import Link from "next/link";
-import { cn } from "@/lib/utils";
 
 export default function ShopPage() {
   return (
@@ -26,33 +23,11 @@ export default function ShopPage() {
             target="_blank"
             rel="noopener noreferrer nofollow"
           >
-            <div className="mb-4">Visit our store . . .</div>
             <img
               src="./shop.png"
               className="bg-card rounded-2xl p-8 border border-border shadow-md hover:shadow-lg transition-all hover:-translate-y-1 cursor-pointer h-full"
             />
           </a>
-        </div>
-        <div className="mb-4">. . .or shop for items right here</div>
-        <div className="grid gap-8 md:grid-cols-1 lg:grid-cols-2">
-          {Object.values(shop).map((item, idx) => {
-            return (
-              <Link href={`/shop/${item.link}`}>
-                <div className="bg-card rounded-2xl p-8 border border-border shadow-md hover:shadow-lg transition-all hover:-translate-y-1 cursor-pointer h-full">
-                  <ShoppingBag
-                    className={cn(
-                      "w-12 h-12 mb-4",
-                      idx % 2 === 0 ? "text-primary" : "text-accent"
-                    )}
-                  />
-                  <h2 className="text-2xl font-bold text-foreground mb-2">
-                    {item.title}
-                  </h2>
-                  <p className="text-foreground/70">{item.subtitle}</p>
-                </div>
-              </Link>
-            );
-          })}
         </div>
       </div>
     </main>
