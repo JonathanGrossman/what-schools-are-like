9a81156e0d067760dfb92df02e19a9dd62c2bc7d
Jonathan Grossman <jon@jonegrossman.com>
2026-01-13 19:07:00 +0200

    separate auth route into lib+route

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,21 @@
 import { signIn, signOut } from "next-auth/react";
 
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
+
 export const handleSignIn = () => signIn("google");
 
 export const handleSignOut = () => signOut();
